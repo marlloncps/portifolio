cbfbdf44e37ca290a91031fc2d2ede0f2930f8d2
marlloncps <marllon.cps@gmail.com>
2023-01-02 02:10:38 -0300

    Adicionando conteudos ao header, e criação do componente nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+import '../css/nav.css';
+
+export default function Nav() {
+  return (
+    <>
+    <section id='nav-section'>
+      <div id='nav-div'>
+      <p>
+        
+        MarllonDev</p>
+      <nav id='nav-header'>
+        <div>
+        <a href='#nav-header'>Início</a>
+        <a href='#nav-header'>Sobre mim</a>
+        <a href='#nav-header'>Projetos</a>
+        <a href='#nav-header'>Contato</a>
+        </div>
+      </nav>
+
+      </div>
+    </section>
+    </>
+  );
+}
